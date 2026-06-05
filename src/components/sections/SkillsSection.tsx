f7e441e6731afc1652933552dfe1e7a9b52462f6
ichitaka58 <ichinosetakayuki92@gmail.com>
2026-06-05 10:54:31 +0900

    SkillsSectionコンポーネントのスキルマッピングでインデックスをキーとして使用するのをやめ、スキル名をキーとして使用するように変更

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -6,8 +6,8 @@ const SkillsSection = () => {
       <div className="max-w-4xl mx-auto p-10">
         <h2 className="text-2xl font-bold">Skills</h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
-          {skills.map((skill, index) => (
-            <div key={index} className="p-4 border border-slate-500 shadow-lg rounded-lg bg-green-100/50 font-bold">{skill}</div>
+          {skills.map((skill) => (
+            <div key={skill} className="p-4 border border-slate-500 shadow-lg rounded-lg bg-green-100/50 font-bold">{skill}</div>
           ))}
         </div>
       </div>
