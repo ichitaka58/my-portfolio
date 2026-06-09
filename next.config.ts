dff2aaea1a466d8454635b0da79de6761c99dc06
ichitaka58 <ichinosetakayuki92@gmail.com>
2026-06-09 22:20:49 +0900

    画像のリモートパターンを追加し、HeroSectionにバッジとGitHub貢献グラフを追加

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://images.microcms-assets.io/assets/**")],
+    remotePatterns: [
+      new URL("https://images.microcms-assets.io/assets/**"),
+      new URL("https://ghchart.rshah.org/ichitaka58"),
+    ],
   },
 };
 
